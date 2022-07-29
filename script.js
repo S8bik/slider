0561399f88d68fbab8dfdf3b2a8a0918db99d25d
S8bik <katryach27@gmail.com>
2022-07-29 14:43:11 +0000

    slider

--- script.js
+++ script.js
@@ -3,14 +3,12 @@ const nextButton = document.querySelector('.next');
 const sliderContainer = document.querySelector('.slider-container');
 const sliderPhotos = Array.from(sliderContainer.children);
 let sliderPosition = 0;
-let index = 0;
 
 function next(){
     if ( sliderContainer.style.left <= -(sliderPhotos.length * 500 + 'px')) {
         sliderContainer.style.left = sliderPosition + 'px';
     }else {
-        let index = sliderPosition + 1;
-        sliderContainer.style.left += (index) * -500 + 'px';
+        sliderContainer.style.left += -500 + 'px';
         sliderContainer.innerHTML += 'a';
     } 
 
